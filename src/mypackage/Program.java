f585c7d712ece52aa231095b4c653b90e4427926
yuvrajgadadare <yuvraj.gadadare@gmail.com>
2025-09-26 16:14:26 +0530

    Initial commit

---
+++ src/mypackage/Program.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class Program {
+
+	public void Display() {
+		System.out.println("Welcome");
+	}
+	public static void main(String[] args) {
+		Program p =new Program();
+		p.Display();
+	}
+}
